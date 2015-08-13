14d7d2d1f3007de8dd085eaddf415c820595bfe9
Edward Jones <ed.jones@embecosm.com>
2015-08-13 09:46:39 +0100

    AAP: Disable frame pointer
    The frame pointer implementation is currently broken, so disable the
    frame pointer by default for now.

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -2487,8 +2487,9 @@ static bool areOptimizationsEnabled(const ArgList &Args) {
 
 static bool shouldUseFramePointerForTarget(const ArgList &Args,
                                            const llvm::Triple &Triple) {
-  // XCore never wants frame pointers, regardless of OS.
-  if (Triple.getArch() == llvm::Triple::xcore) {
+  // XCore and AAP never want frame pointers, regardless of OS.
+  if ((Triple.getArch() == llvm::Triple::aap) ||
+      (Triple.getArch() == llvm::Triple::xcore)) {
     return false;
   }
 
